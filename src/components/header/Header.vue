2e3c9e5af2cbcb151a3f468af4279641f9902782
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2018-01-23 13:03:58 +0200

    Clean-up Header.

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -20,27 +20,21 @@ export default {
 <style scoped>
   
   header {
+    color: white;
+    background: #2c3e50;
     height: 100px;
+    width: 100%;
+    margin: 0;
+   
     display: grid;
     grid-template-columns: 3fr 7fr;
     grid-template-rows: 100%;
-    color: white;
-    /* text-align: left; */
     align-items: center;
     justify-items: center;
-    width: 100%;
-    background: #2c3e50;
-    margin: 0;
-    /* padding: 20px 10px; */
   }
 
   header h1.title {
-    /* display: inline-block; */
-    /* background: red; */
     justify-self: start;
-    /* align-self: end; */
-    /* text-align: center; */
-    /* height: 100%; */
     margin: 0 0 0 20px;
     padding: 10px;
   }  
