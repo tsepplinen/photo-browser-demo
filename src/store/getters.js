79a824d4e3031fee95ada790fe5ad92edf93e44b
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2018-01-23 13:20:55 +0200

    Clean-up getters.

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,11 +1,10 @@
+// Finds a photo stored in albums
 const findPhotoFromAlbums = (id, albumId, albums) => {
-  console.log("findPhotoFromAlbums", id, albumId, albums);
   
   let photo = null;
   try {
     photo = albums[albumId].photos.find((photo) => photo.id == id);
   } catch (error) {
-    console.log("Error finding photo from album:", error);
     return null;
   }
 
@@ -14,108 +13,71 @@ const findPhotoFromAlbums = (id, albumId, albums) => {
       const pages = albums[albumId].pages;
       photo = findPhotoFromPages(id, pages);
     } catch (error) {
-      console.log("Error finding photo from album page", error);
+      // Do nothing
     }
   }
 
   return photo;
-
-  // const photos = albums[albumId].photos;
-  // if (photos) {
-  //   return photos.find;
-  // } else {
-  //   return undefined;
-  // }
 }
 
+// Finds a photo stored in pages
 const findPhotoFromPages = (id, pages) => {
-  console.log("findPhotoFromPages", id);
-  
   let photo = null;
   
   if (pages) {
-    console.log("pages true", pages);
-  
     for(const page of pages) {
-      console.log("page", page);
       if (page) {
         const found = page.find(item => Number(item.id) === id);
         if (found) {
-          console.log("Found", found);
           photo = found;
           break;
         }
       }
     }
-
-    // pages.some((page) => {
-    //   const found = page.find(item => Number(item.id) === id);
-    //   if (found) {
-    //     console.log("Found", found);
-    //     photo = found;
-    //     return true;
-    //   } else {
-    //     console.log("not found");
-    //     return false;
-    //   }
-    // });
   }
   return photo;
 };
 
 export default {
-  getTest: (state) => () => {
-    console.log("getter test");
-    return state.test;
-  },
 
+
+  // Gets photo by id from albums or pages
   getPhotoById: (state) => (idToFind, albumId) => {
-    console.log('Store: getPhotoById', idToFind, albumId);
     const id = Number(idToFind);
 
     let photo = findPhotoFromAlbums(id, albumId, state.albums);
     if (!photo) {
       photo = findPhotoFromPages(id, state.photoPages);
     } 
-    console.dir(photo);
+
+    // If photos albumId does not match
     if (photo && (photo.albumId != albumId)) {
-      console.log("getPhotoById errori", state);
       state.error.photoError = true;
     }
     return photo;
   },
+
+  // Gets a page of photos from store.
   getPhotoPage: (state) => (pageNum) => {
     console.log('Store: getPhotoPage', pageNum);
     const page = state.photoPages[pageNum];
     console.dir(page);
     return page;
   },
-  getAlbumById: (state) => (id) => {
-    console.log('Store: getAlbumById');
-    
-    // const album = state.albums.find((album) => {
-    //   if (album && album.info) {
-    //     const albumId = album.info.id;
-    //     return (Number(albumId) === Number(id))
-    //   } else {
-    //     return false;
-    //   }
-    // });
 
+  // Gets an album by id from store
+  getAlbumById: (state) => (id) => {
     const album = state.albums[id];
     if (album && album.info) {
       if (album.info.id != id) {
         throw "Error: album id does not match its index."
       }
     }
-    console.dir(album);
     return album;
   },
+
+  // Gets a page of albums for album listing
   getAlbumListPage: (state) => (page) => {
-    console.log('getAlbumListPage', page);
     return state.albumPages[page];
   },
-  lastLoadedPhoto: (state) => () => {
-    return state.photos.length;
-  }
 }
