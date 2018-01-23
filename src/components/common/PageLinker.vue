44eb6160f06cdbc31809d321d91ceaefdfefe0e7
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2018-01-23 13:02:19 +0200

    Clean-up PageLinker.

--- src/components/common/PageLinker.vue
+++ src/components/common/PageLinker.vue
@@ -1,11 +1,22 @@
 <template>
   <div v-if="hasPages" class="container">
     <router-link v-if="hasPrevious" :to="linkTo(previous)">Previous</router-link>
-    <router-link v-for="page in begin" :key="page" class="page" :to="linkTo(page)" v-bind:class="{current: isCurrent(page)}">{{page}}</router-link>
+
+    <router-link v-for="page in begin" :key="page"
+                 class="page" :to="linkTo(page)"
+                 v-bind:class="{current: isCurrent(page)}">{{page}}</router-link>
+
     <span v-if="showFirstDots" class="dots">...</span>
-    <router-link v-if="showCurrent" class="page current" :to="linkTo(currentPage)">{{currentPage}}</router-link>
+
+    <router-link v-if="showCurrent" class="page current"
+                 :to="linkTo(currentPage)">{{currentPage}}</router-link>
+
     <span v-if="showLastDots" class="dots">...</span>
-    <router-link v-for="page in end" :key="page" class="page" :to="linkTo(page)" v-bind:class="{current: isCurrent(page)}">{{page}}</router-link>
+
+    <router-link v-for="page in end" :key="page" class="page"
+                 :to="linkTo(page)"
+                 v-bind:class="{current: isCurrent(page)}">{{page}}</router-link>
+
     <router-link v-if="hasNext" :to="linkTo(next)">Next</router-link>
   </div>
 </template>
@@ -16,20 +27,22 @@ export default {
   props: ['currentPage','first', 'last', 'linkBuilder'],
   data() {
     return {
+      // Until this many pages, use simple mode
       simpleLimit: 7,
     }
   },  
   computed: {
+    // Tells if more than one page exists
     hasPages() {
       return this.pageCount > 1;
     },
     pageCount() {
       return (this.last - this.first) + 1;
-
     },
     isSimple() {
       return this.pageCount <= this.simpleLimit;
     },
+    // Page buttons from beginning.
     begin() {
       const pageCount = this.pageCount;
       let pageArray = [];
@@ -42,6 +55,7 @@ export default {
       }
       return pageArray;
     },
+    // Page buttons from end.
     end() {
       const pageCount = this.pageCount;
       if (pageCount > this.simpleLimit) {  
@@ -64,6 +78,7 @@ export default {
         return this.currentPage < Number(this.last) - 3;
       }
     },
+    // Should current page should be shown in middle
     showCurrent() {
       if (this.isSimple) {
         return false;
@@ -72,33 +87,33 @@ export default {
       const min = Number(this.first) + 2;
       const max = Number(this.last) - 2;
       const value = (min < page && page < max);
-      console.log("show current", value);
     
       return value;
     },
+    // Tells is there a previous page
     hasPrevious() {
-      console.log("has previous", this.previous);
       return (Number(this.previous) >= this.first);
     },
+    // Tells is there a next page
     hasNext() {
-      console.log("has next", this.next);
       return (Number(this.next) <= this.last);
     },
+    // Previous pages number
     previous() {
       return Number(this.currentPage) - 1;
     },
+    // Next pages number
     next() {
       return Number(this.currentPage) + 1;
     },
   },
   methods: {
+    // Creates a link to page
     linkTo(page) {
       return this.linkBuilder(page);
-      // return `/photos?page=${page}`;
     },
+    // Check if a page is the current page
     isCurrent(page) {
-      console.log("iscurrent", page, this.currentPage);
-      
       return Number(page) === Number(this.currentPage);
     }
   }
@@ -107,7 +122,6 @@ export default {
 
 <style scoped>
   a.page, a.current {
-    /* padding: 10px 15px; */
     margin: 0 2px;
     background: lightblue;
     border-radius: 7px;
